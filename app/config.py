16d4e4b966b96bb600ac7333bfe5e81b068fc12d
amirmohamadred4-dev <amirmohamadred4@gmail.com>
2026-09-08 10:01:24 +0330

    Add environment variable configurations to config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,8 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7390580427"))
+
+TIMEZONE = os.getenv("TIMEZONE", "Asia/Tehran")
